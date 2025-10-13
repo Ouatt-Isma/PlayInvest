b6591170bcc2fc966fb627bdcb03213c01439b3e
Ouatt-Isma <koffi-ismael.ouattara@polytechnique.edu>
2025-10-13 15:09:20 +0200

    deploy

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,9 @@
 export default defineNuxtConfig({
-  runtimeConfig:{public: {
-    apiBase: process.env.API_BASE_URL || 'http://localhost:8000'
-  }},
+  runtimeConfig:{
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000'
+    }
+  },
   vite: {
     server: {
       allowedHosts: [
